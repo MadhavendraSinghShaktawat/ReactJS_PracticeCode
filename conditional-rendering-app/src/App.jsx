7d9cac24da6ede233dfb90e1ec6034eb2cedde15
MadhavendraSinghShaktawat <madhavendrasinghshaktawat@gmail.com>
2024-02-17 00:58:36 +0530

    adding-userGreeting

--- conditional-rendering-app/src/App.jsx
+++ conditional-rendering-app/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <UserGreetings isLoggedIn={true} username="BroCode"/>
+      <UserGreetings />
     </>
   )
 }
--- conditional-rendering-app/src/UserGreetings.jsx
+++ conditional-rendering-app/src/UserGreetings.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function UserGreetings(props){    
     // if(props.isLoggedIn){
     //     return(
@@ -12,7 +14,17 @@ function UserGreetings(props){
     //         </div>
     //     );
     // }
-    return(props.isLoggedIn ? <h1 className="welcome-message">Welcome {props.username}</h1>:<h1 className="login-prompt">Please Logg In to continue</h1>)
+    const WelcomeMSG = <h1 className="welcome-message">Welcome {props.username}</h1>
+    const LoginMSG = <h1 className="login-prompt">Please Logg In to continue</h1>
+    return(props.isLoggedIn ? WelcomeMSG : LoginMSG)
+}
+UserGreetings.prototypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string,
+}
+UserGreetings.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest"
 }
 
 export default UserGreetings
