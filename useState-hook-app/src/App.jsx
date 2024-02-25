8aff4336ec8318cbfab2e250ad73ff3d4068235c
MadhavendraSinghShaktawat <madhavendrasinghshaktawat@gmail.com>
2024-02-26 00:17:20 +0530

    adding useState-hook-app

---
+++ useState-hook-app/src/App.jsx
@@ -0,0 +1,18 @@
+// React hook = Special function that allows functional components
+//              to use React features without writing class components (Change made in react v16.8)
+//              (useState, useEffect, useContext, useReducer, useCallback, and many more...)
+
+// useState() = A react hook that allows the creation of a statful variable
+//              and a setter function to update its value in the Virtual DOM
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
